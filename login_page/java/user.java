2fe41c1ecd9458f66acf3289d94fd07982c05823
ashlw <alwang@usc.edu>
2021-10-24 15:41:11 -0700

    user class

---
+++ login_page/java/user.java
@@ -0,0 +1,15 @@
+package login_page.java; 
+public class user{
+    private int id; 
+    private String username; 
+    private String password; 
+
+    public user(int id, String username, String password){
+        this.id = id; 
+        this.username = username; 
+        this.password = password; 
+    }
+
+
+
+}
